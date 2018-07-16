1351568f7698fcf208320d0495d95ad3b98c6a62
lasithaccna <lasithaccna@gmail.com>
2018-07-17 00:23:12 +0530

    Add files via upload

---
+++ COHDCN181P-008.py
@@ -0,0 +1,17 @@
+Total=0
+
+while True:
+
+	x=input("Enter number here :")
+
+	if x=='':
+
+		break
+    
+	try:
+		Total=Total+int(x)
+
+	except ValueError:
+		continue
+
+print("The Total Is ",Total)
